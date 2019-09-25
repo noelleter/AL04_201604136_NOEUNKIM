3304e3f7c40f5d1af8c98fb9e442e98f7876ac03
noelleter <shdmsrla@gmail.com>
2019-09-25 18:48:49 +0900

    AL04

---
+++ src/app/Main_AL04_201604136.java
@@ -0,0 +1,8 @@
+package app;
+
+public class Main_AL04_201604136 {
+    public static void main(String[] args) {
+        AppController appController = new AppController();
+        appController.run();
+    }
+}
